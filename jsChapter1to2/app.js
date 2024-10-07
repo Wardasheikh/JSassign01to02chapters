198ecc5830d3e1459d37742d901b88bc7950ad6a
WardahIrshad <wardahirshad117@gmail.com>
2024-10-07 11:26:03 +0500

    Add files via upload

---
+++ jsChapter1to2/app.js
@@ -0,0 +1,46 @@
+//*******CHAPTER 01*****
+ // Ans 1
+ // alert is a function to display the message for user and show the popup for user.
+//  Ans 02
+ // alert("Welcome to my website!")
+ // Ans 03
+ // ALERT write always small letter and if you write that with capitalize
+ //  popup will not show then ,because it is predefined function you cannot changes it ,because it is case-sensitive.
+  // Ans 04
+ // alert ("Hello, world!");
+ //  Ans 05
+
+ // alert function name (case sensitive) you cannot write keyword name
+ // alert is pre-defined function function recognize with round bracket ()
+ // use letters always with String ""
+ // Exclamation mark use for emphasis to the message! 
+ // statement terminator used for end statement;
+ // declare message and assign Value.
+
+//******** CHAPTER 02 ********
+ // Q1
+var text = "hello world!";
+alert(text);
+
+ // Q2
+var age;
+age=25;
+alert(age);
+// Q3
+var greeting = "Good morning!";
+alert(greeting);
+
+greeting = "Good evening!";
+alert(greeting);
+//  Q4
+
+var favouriteColor = "blue";
+alert("my favourite color is"+favouritecolor);
+// Q5
+var age ;
+age =25;
+alert(age);
+
+//****End****
+
+
